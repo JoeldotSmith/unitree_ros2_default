2e2c51916f8d5f4d2b7c4666b181b66c63bd4263
Joel Smith <23338559@student.uwa.edu.au>
2025-02-28 14:53:37 +0800

    + simpler (not finished)

--- setup.sh
+++ setup.sh
@@ -2,9 +2,11 @@
 echo "Setup unitree ros2 environment"
 source /opt/ros/foxy/setup.bash
 echo "Setup unitree cyclonedds_ws"
-source $HOME/unitree_ros2/cyclonedds_ws/install/setup.bash
+source $HOME/unitree_ros2_default/cyclonedds_ws/install/setup.bash
+echo "Setup unitree go/api/hg"
+source $HOME/unitree_ros2_default/install/setup.bash
 echo "Setup unitree ws_livox"
-source $HOME/unitree_ros2/ws_livox/install/setup.bash
+source $HOME/unitree_ros2_default/ws_livox/install/setup.sh
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 export CYCLONEDDS_URI='<CycloneDDS><Domain><General><Interfaces>
                             <NetworkInterface name="enx00e04c680157" priority="default" multicast="default" />
